ee33af5713e03f903b8c03f0342482389b75ab21
raijinthunderkeg <lis892009@gmail.com>
2013-09-17 22:06:19 +0400

    Algorithm implementation

---
+++ LuhnAlgorithm/LuhnAlgorithm/LuhnChecker.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuhnAlgorithm
+{
+    class LuhnChecker
+    {
+        public static bool Check(String number)
+        {
+            int i = number.Length;
+            int sum = 0;
+            while (i > 0)
+            {
+                int element = int.Parse(number.Substring(i-1,1));
+                if ((i-1) % 2 == 0)
+                {
+                    element *= 2;
+                    element = element>9?element/10+element%10:element;
+                }
+                sum += element;
+                i--;
+            }
+            return sum%10==0;
+        }
+    }
+}
---
+++ LuhnAlgorithm/LuhnAlgorithm/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuhnAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //4  5  6  1     2  6  1  2     1  2  3  4     5  4  6  4
+            String firstNumber = "4561261212345464";
+            Console.WriteLine("Check "+firstNumber + " : "+(LuhnChecker.Check(firstNumber)?"valid":"invalid") );
+            //4  5  6  1     2  6  1  2     1  2  3  4     5  4  6  7
+            firstNumber = "4561261212345467";
+            Console.WriteLine("Check " + firstNumber + " : " + (LuhnChecker.Check(firstNumber) ? "valid" : "invalid"));
+        }
+    }
+}
